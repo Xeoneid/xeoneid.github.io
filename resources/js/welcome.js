e89e3d21cecbe44aa2f8482867f4b49ef4cfc311
Blas <blas00go@gmail.com>
2017-01-25 19:29:13 -0300

    div#welcome display:none; cuando ya no se usa

--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -8,8 +8,17 @@ function carousel() {
       x[i].style.display = "none"; 
     }
     slideIndex++;
+    console.log("slideIndex esta en " + slideIndex);
+    if (slideIndex == 5) {
+      // este if saca el div#welcome
+      console.log("entro al if para sacar div#welcome");
+      document.getElementById("welcome").style.display = "none";
+      console.log("done");
+    }
     //if (slideIndex == x.length+1) {window.location.href = "main.htm";}
     if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2500);
+    x[slideIndex-1].style.display = "block";
+    if (slideIndex + 1 <= x.length) {
+      setTimeout(carousel, 2000);
+    }
 }
